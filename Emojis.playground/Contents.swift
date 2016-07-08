b0a0e8a378f5c3f0c6e788cfce7512e84f170623
carsonlevine <carsonglevine@me.com>
2016-07-08 15:20:31 -0400

    first commit

---
+++ Emojis.playground/Contents.swift
@@ -0,0 +1,75 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Level 1
+//"Create a function called emojis that accepts a string parameter. Depending on the paramter, the function will return a different emoji. The emoji conversion goes as follows:
+//happy -> :)
+//sad -> :(
+//group -> (-_(-_(-_(-_-)_-)_-)_-)
+//happiest -> :D
+//shrugging -> ¯\_(ツ)_/¯
+//sunglasses -> =_=
+//ears -> @(^_^)@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------
+//Level 2
+//Read about switch statements here: https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/ControlFlow.html#//apple_ref/doc/uid/TP40014097-CH9-ID127 (scroll down below the if statements)
+//change your logic control in your function to use a switch statement instead of if/else if/else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------
+//Level 3
+//If the parameter doesn't match up with the list of options, pick a random element and return that value.
+//You can read about generating random values here: http://stackoverflow.com/a/24052491/5438610
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
